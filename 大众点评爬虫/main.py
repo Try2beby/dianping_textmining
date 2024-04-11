a7644eaf94db67f1d97900c3916d4c47e4ca9c19
Try2bebtr <whtu23@m.fudan.edu.cn>
2024-04-11 09:48:36 +0000

    init commit

--- 大众点评爬虫/main.py
+++ 大众点评爬虫/main.py
@@ -5,7 +5,7 @@ Created on Mon Jul  9 16:42:52 2018
 @author: bin
 """
 
-#目标爬取店铺的评论
+# 目标爬取店铺的评论
 
 import requests
 from bs4 import BeautifulSoup
@@ -14,36 +14,48 @@ import mysqls
 import re
 from fake_useragent import UserAgent
 import os
+import json
+
+cacheDir = "./cache/"
 
 ua = UserAgent()
 
-#设置cookies
-cookie = "_lxsdk_cuid=162760423dfc8-0801f141cb0731-3b60490d-e1000-162760423dfc8; _lxsdk=162760423dfc8-0801f141cb0731-3b60490d-e1000-162760423dfc8; _hc.v=af7219c3-2b99-8bb8-f9b2-7b1d9be7f29e.1522398406; s_ViewType=10; ua=%E4%BB%A4%E7%8B%90%E5%86%B2; ctu=029e953356caf94d20233d299a70d285a03cb64585c371690b17d3e59c4c075c; cye=guangzhou; Hm_lvt_e6f449471d3527d58c46e24efb4c343e=1531964746; cy=4; dper=8c6ae023e893759ea57ce154028f1800be56b69450806b893b9cf5c6b6c3e3ba3c986c9a603bcbf9a7fb18dcd2038cf704b3e3baba3532bc7dffec965fe5e6c3b2479ca21c6577a1f5636088acbba8936df6ac994e02a923a907907a938559f9; ll=7fd06e815b796be3df069dec7836c3df; _lx_utm=utm_source%3DBaidu%26utm_medium%3Dorganic; _lxsdk_s=1661889a264-50e-66f-22a%7C%7C276"
+# 读取JSON文件中的Cookies
+with open(os.path.join(cacheDir, "cookies.json"), "r") as f:
+    cookies = json.load(f)
 
-#修改请求头
+# 构造Cookies字符串
+cookie = "; ".join([f"{item['name']}={item['value']}" for item in cookies])
+
+# 修改请求头
 headers = {
-        'User-Agent':ua.random,
-        'Cookie':cookie,
-        'Connection':'keep-alive',
-        'Host':'www.dianping.com',
-        'Referer': 'http://www.dianping.com/shop/521698/review_all/p6'
+    "User-Agent": ua.random,
+    "Cookie": cookie,
+    "Connection": "keep-alive",
+    "Host": "www.dianping.com",
+    "Referer": "http://www.dianping.com/shop/l9qwmkX3FoD9tExc/review_all",
 }
 
-#从ip代理池中随机获取ip
-#ips = open('proxies.txt','r').read().split('\n')
-#
-#def get_random_ip():
-#    ip = random.choice(ips)
-#    pxs = {ip.split(':')[0]:ip}
-#    return pxs
+# # 从ip代理池中随机获取ip
+# ips = open("proxies.txt", "r").read().split("\n")
 
-#获取html页面
-def getHTMLText(url,code="utf-8"):
+
+# def get_random_ip():
+#     ip = random.choice(ips)
+#     pxs = {ip.split(":")[0]: ip}
+#     return pxs
+
+
+# 获取html页面
+def getHTMLText(url, code="utf-8"):
     try:
-        time.sleep(random.random()*6 + 2)
-        r=requests.get(url, timeout = 5, headers=headers, 
-#                       proxies=get_random_ip()
-                       )
+        time.sleep(random.random() * 6 + 2)
+        r = requests.get(
+            url,
+            timeout=5,
+            headers=headers,
+            #                       proxies=get_random_ip()
+        )
         r.raise_for_status()
         r.encoding = code
         return r.text
@@ -51,86 +63,109 @@ def getHTMLText(url,code="utf-8"):
         print("产生异常")
         return "产生异常"
 
-#因为评论中带有emoji表情，是4个字符长度的，mysql数据库不支持4个字符长度，因此要进行过滤
+
+# 因为评论中带有emoji表情，是4个字符长度的，mysql数据库不支持4个字符长度，因此要进行过滤
 def remove_emoji(text):
     try:
-        highpoints = re.compile(u'[\U00010000-\U0010ffff]')
+        highpoints = re.compile("[\U00010000-\U0010ffff]")
     except re.error:
-        highpoints = re.compile(u'[\uD800-\uDBFF][\uDC00-\uDFFF]')
-    return highpoints.sub(u'',text)
+        highpoints = re.compile("[\uD800-\uDBFF][\uDC00-\uDFFF]")
+    return highpoints.sub("", text)
 
-#从html中提起所需字段信息
-def parsePage(html,shpoID):
-    infoList = [] #用于存储提取后的信息，列表的每一项都是一个字典
+
+# 从html中提起所需字段信息
+def parsePage(html, shpoID):
+    infoList = []  # 用于存储提取后的信息，列表的每一项都是一个字典
     soup = BeautifulSoup(html, "html.parser")
-    
-    for item in soup('div','main-review'):
-        cus_id = item.find('a','name').text.strip()
-        comment_time = item.find('span','time').text.strip()
+
+    # # 找到所有页码的链接
+    # page_links = soup.find_all('a', class_='PageLink')
+
+    # # 假设最后一个PageLink包含的是最大页数
+    # total_pages = int(page_links[-1].text) if page_links else 0
+
+    for item in soup("div", "main-review"):
+        cus_id = item.find("a", "name").text.strip()
+        comment_time = item.find("span", "time").text.strip()
         try:
-            comment_star = item.find('span',re.compile('sml-rank-stars')).get('class')[1]
+            comment_star = item.find("span", re.compile("sml-rank-stars")).get("class")[
+                1
+            ]
         except:
-            comment_star = 'NAN'
-        cus_comment = item.find('div',"review-words").text.strip()
-        scores = str(item.find('span','score'))
+            comment_star = "NAN"
+        cus_comment = item.find("div", "review-words").text.strip()
+        scores = str(item.find("span", "score"))
         try:
-            kouwei = re.findall(r'口味：([\u4e00-\u9fa5]*)',scores)[0]
-            huanjing = re.findall(r'环境：([\u4e00-\u9fa5]*)',scores)[0]
-            fuwu = re.findall(r'服务：([\u4e00-\u9fa5]*)',scores)[0]
+            kouwei = re.findall(r"口味：([\u4e00-\u9fa5]*)", scores)[0]
+            huanjing = re.findall(r"环境：([\u4e00-\u9fa5]*)", scores)[0]
+            fuwu = re.findall(r"服务：([\u4e00-\u9fa5]*)", scores)[0]
+            shicai = re.findall(r"食材：([\u4e00-\u9fa5]*)", scores)[0]
         except:
-            kouwei = huanjing = fuwu = '无'
-        
-        infoList.append({'cus_id':cus_id,
-                         'comment_time':comment_time,
-                         'comment_star':comment_star,
-                         'cus_comment':remove_emoji(cus_comment),
-                         'kouwei':kouwei,
-                         'huanjing':huanjing,
-                         'fuwu':fuwu,
-                         'shopID':shpoID})
+            kouwei = huanjing = fuwu = shicai = "NAN"
+
+        infoList.append(
+            {
+                "cus_id": cus_id,
+                "comment_time": comment_time,
+                "comment_star": comment_star,
+                "cus_comment": remove_emoji(cus_comment),
+                "kouwei": kouwei,
+                "huanjing": huanjing,
+                "fuwu": fuwu,
+                "shicai": shicai,
+                "shopID": shpoID,
+            }
+        )
     return infoList
 
-#构造每一页的url，并且对爬取的信息进行存储
+
+# 构造每一页的url，并且对爬取的信息进行存储
 def getCommentinfo(shop_url, shpoID, page_begin, page_end):
     for i in range(page_begin, page_end):
         try:
-            url = shop_url + 'p' + str(i)
+            url = shop_url + "p" + str(i)
             html = getHTMLText(url)
-            infoList = parsePage(html,shpoID)
-            print('成功爬取第{}页数据,有评论{}条'.format(i,len(infoList)))
+            infoList = parsePage(html, shpoID)
+            print("成功爬取第{}页数据,有评论{}条".format(i, len(infoList)))
             for info in infoList:
                 mysqls.save_data(info)
-            #断点续传中的断点
+            time.sleep(random.randint(5, 12))
+            # 断点续传中的断点
             if (html != "产生异常") and (len(infoList) != 0):
-                with open('xuchuan.txt','a') as file:
-                    duandian = str(i)+'\n'
-                    file.write(duandian)
+                download_info = {"nowpage": i}
+                with open(os.path.join(cacheDir, "download_info.json"), "w") as f:
+                    json.dump(download_info, f)
             else:
-                print('休息60s...')
+                print("休息60s...")
                 time.sleep(60)
         except:
-            print('跳过本次')
+            print("跳过本次")
             continue
     return
 
-def xuchuan():
-    if os.path.exists('xuchuan.txt'):
-        file = open('xuchuan.txt','r')
-        nowpage = int(file.readlines()[-1])
-        file.close()
+
+def get_download_info():
+    if os.path.exists(os.path.join(cacheDir, "download_info.json")):
+        with open(os.path.join(cacheDir, "download_info.json"), "r") as f:
+            download_info = json.load(f)
+    else:
+        download_info = {}
+    return download_info
+
+
+# 根据店铺id，店铺页码进行爬取
+def craw_comment(shopID="l9qwmkX3FoD9tExc", page=1064):
+    shop_url = "http://www.dianping.com/shop/" + shopID + "/review_all/"
+    # 读取断点续传中的续传断点
+    download_info = get_download_info()
+    if download_info:
+        nowpage = download_info["nowpage"]
     else:
         nowpage = 0
-    return nowpage
-
-#根据店铺id，店铺页码进行爬取
-def craw_comment(shopID='521698',page = 53):
-    shop_url = "http://www.dianping.com/shop/" + shopID + "/review_all/"
-    #读取断点续传中的续传断点
-    nowpage = xuchuan()
-    getCommentinfo(shop_url, shopID, page_begin=nowpage+1, page_end=page+1)
+    getCommentinfo(shop_url, shopID, page_begin=nowpage + 1, page_end=page + 1)
     mysqls.close_sql()
     return
 
+
 if __name__ == "__main__":
     craw_comment()
-        
--- 大众点评爬虫/mysqls.py
+++ 大众点评爬虫/mysqls.py
@@ -7,14 +7,21 @@ Created on Tue Jul 24 15:45:05 2018
 
 import pymysql
 
-#连接MYSQL数据库
-db = pymysql.connect("localhost","root","","TESTDB" )
+# 连接MYSQL数据库
+db = pymysql.connect(
+    host="localhost",  # 数据库地址，本地为localhost
+    user="root",  # 数据库用户
+    password="2902054141",  # 用户密码
+    database="testdb",  # 要连接的数据库名
+    charset="utf8mb4",  # 字符编码
+)
 cursor = db.cursor()
 
-#在数据库建表
+
+# 在数据库建表
 def creat_table():
     cursor.execute("DROP TABLE IF EXISTS DZDP")
-    sql = '''CREATE TABLE DZDP(
+    sql = """CREATE TABLE DZDP(
             cus_id varchar(100),
             comment_time varchar(55),
             comment_star varchar(55),
@@ -22,30 +29,35 @@ def creat_table():
             kouwei varchar(55),
             huanjing varchar(55),
             fuwu varchar(55),
+            shicai varchar(55),
             shopID varchar(55)
-            );'''
+            );"""
     cursor.execute(sql)
     return
 
-#存储爬取到的数据
+
+# 存储爬取到的数据
 def save_data(data_dict):
-    sql = '''INSERT INTO DZDP(cus_id,comment_time,comment_star,cus_comment,kouwei,huanjing,fuwu,shopID) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)'''
-    value_tup = (data_dict['cus_id']
-                 ,data_dict['comment_time']
-                 ,data_dict['comment_star']
-                 ,data_dict['cus_comment']
-                 ,data_dict['kouwei']
-                 ,data_dict['huanjing']
-                 ,data_dict['fuwu']
-                 ,data_dict['shopID']
-                 )
+    sql = """INSERT INTO DZDP(cus_id,comment_time,comment_star,cus_comment,kouwei,huanjing,fuwu,shicai,shopID) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    value_tup = (
+        data_dict["cus_id"],
+        data_dict["comment_time"],
+        data_dict["comment_star"],
+        data_dict["cus_comment"],
+        data_dict["kouwei"],
+        data_dict["huanjing"],
+        data_dict["fuwu"],
+        data_dict["shicai"],
+        data_dict["shopID"],
+    )
     try:
-        cursor.execute(sql,value_tup)
+        cursor.execute(sql, value_tup)
         db.commit()
     except:
-        print('数据库写入失败')
+        print("数据库写入失败")
     return
 
-#关闭数据库
+
+# 关闭数据库
 def close_sql():
     db.close()
